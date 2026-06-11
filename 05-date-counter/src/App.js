980d2e8eaebb40320b456b2af6006d69a52aaee5
wakabibrian <wakabibrian95@gmail.com>
2026-06-11 21:38:21 +0300

    Complete date-counter exercise

--- 05-date-counter/src/App.js
+++ 05-date-counter/src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
+  // const [date, setDate] = useState();
+
+  function addDays(date) {
+    const result = new Date(date);
+    result.setDate(result.getDate() + count);
+    return result;
+  }
+
+  // const days = setDate(date);
+  const now = addDays("2027-06-21");
+
+  const date = now.toDateString();
+
+  function text() {
+    let result = "Today is";
+    if (count > 0) result = `${count} days from today is`;
+    if (count < 0) result = `${Math.abs(count)} days ago was`;
+    return result;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="step">
+        <button onClick={() => setStep((step) => step - 1)}>-</button>
+        <p>Step: {step}</p>
+        <button onClick={() => setStep((step) => step + 1)}>+</button>
+      </div>
+      <div className="count">
+        <button onClick={() => setCount((count) => count - step)}>-</button>
+        <p>Count: {count}</p>
+        <button onClick={() => setCount((count) => count + step)}>+</button>
+      </div>
+      <p className="date">{`${text()} ${date}`}</p>
     </div>
   );
 }
